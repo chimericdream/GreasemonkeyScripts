77bfa858f389df6fa6f95b0e8f9e11311e3ec6ef
Bill Parrott <william.parrott@lexmark.com>
2015-09-08 13:53:47 -0500

    fixing match url

--- dist/twitter-skip-to-image/twitter-skip-to-image.user.js
+++ dist/twitter-skip-to-image/twitter-skip-to-image.user.js
@@ -4,7 +4,7 @@
 // @version     1.0.0
 // @updateUrl   https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/twitter-skip-to-image/twitter-skip-to-image.meta.js
 // @downloadUrl https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/twitter-skip-to-image/twitter-skip-to-image.user.js
-// @match       /^https?:\/\/([^.]+\.)?twitter\.com\/.*\/status\/\d+\/photo.*$/
+// @match       /^https?://([^.]+\.)?twitter\.com/.*/status/\d+/photo.*$/
 // @require     http://code.jquery.com/jquery-latest.js
 // @grant       none
 // ==/UserScript==
