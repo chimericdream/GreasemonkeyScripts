cefa6b4d2790e8eddead35a245835b5e81823a8f
Bill Parrott <william.parrott@lexmark.com>
2016-05-25 09:57:34 -0500

    not quite working yet

---
+++ dist/feedly-unread-count/feedly-unread-count.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Feedly Unread Count
+// @description Puts the unread count in the favicon for Feedly as well as the browser's title bar
+// @version     2.0.0
+// @updateUrl   https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/feedly-unread-count/feedly-unread-count.meta.js
+// @downloadUrl https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/feedly-unread-count/feedly-unread-count.user.js
+// @include     http://feedly.com/*
+// @include     https://feedly.com/*
+// @require     http://code.jquery.com/jquery-latest.js
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_registerMenuCommand
+// ==/UserScript==
