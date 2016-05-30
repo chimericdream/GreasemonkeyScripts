83e6fbd75ef77a200be4c19b3d22e27a64f9eb74
Bill Parrott <bill@chimericdream.com>
2016-05-29 22:52:41 -0500

    uglify scripts and bump to patch version

--- dist/auto-download-instructables/auto-download-instructables.user.js
+++ dist/auto-download-instructables/auto-download-instructables.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Auto-Download Instructables
 // @description Greasemonkey script to automatically navigate to the download page for an Instructable and trigger the download.
-// @version     1.1.0
+// @version     1.1.1
 // @updateUrl   https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/auto-download-instructables/auto-download-instructables.meta.js
 // @downloadUrl https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/auto-download-instructables/auto-download-instructables.user.js
 // @include     http://instructables.com/id/*
@@ -12,39 +12,4 @@
 // @require     https://raw.githubusercontent.com/chimericdream/JS-URL-Helpers/master/absurl.js
 // @grant       none
 // ==/UserScript==
-(function($, getAbsoluteUrl, getQueryVariable) {
-    'use strict';
-
-    $(document).ready(function() {
-        var delay = function() {
-            var loggedin = ($('#you-menu > a.name').length > 0);
-            if (!loggedin) {
-                return;
-            }
-
-            function goToDownloadPage() {
-                var href = $('#ible-header-actions-bar #download-pdf-btn-top').attr('href');
-                if (typeof href !== 'undefined') {
-                    window.location = getAbsoluteUrl(href);
-                }
-            }
-
-            function downloadIble() {
-                var link = $('#pdf-content > .pdf-download-available > p > a');
-                var href = link.attr('href');
-                if (typeof href !== 'undefined') {
-                    link.css('text-decoration', 'line-through');
-                    window.location = getAbsoluteUrl(href);
-                }
-            }
-
-            var onDownloadPage = (getQueryVariable('download') === 'pdf');
-            if (onDownloadPage) {
-                downloadIble();
-            } else {
-                goToDownloadPage();
-            }
-        };
-        window.setTimeout(delay, 10000);
-    });
-})(jQuery, window.fnGetAbsUrl, window.fnGetQueryVar);
+!function(n,o,t){"use strict";n(document).ready(function(){var e=function(){function e(){var t=n("#ible-header-actions-bar #download-pdf-btn-top").attr("href");"undefined"!=typeof t&&(window.location=o(t))}function a(){var t=n("#pdf-content > .pdf-download-available > p > a"),e=t.attr("href");"undefined"!=typeof e&&(t.css("text-decoration","line-through"),window.location=o(e))}var d=n("#you-menu > a.name").length>0;if(d){var i="pdf"===t("download");i?a():e()}};window.setTimeout(e,1e4)})}(jQuery,window.fnGetAbsUrl,window.fnGetQueryVar);
