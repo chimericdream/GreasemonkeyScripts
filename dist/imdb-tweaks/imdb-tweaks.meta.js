28398a618ef8aa6237dc0a8e41c02c26c9b85960
Bill Parrott <bill@chimericdream.com>
2016-05-31 23:18:15 -0500

    [feature/imdb-tweaks] imdb script not quite fixed

---
+++ dist/imdb-tweaks/imdb-tweaks.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        IMDb Tweaks
+// @description Tweaks to the IMDB website. Remove ref query string and make images linkable.
+// @version     1.0.1
+// @updateUrl   https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/imdb-tweaks/imdb-tweaks.meta.js
+// @downloadUrl https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/imdb-tweaks/imdb-tweaks.user.js
+// @include     /^https?://([^.]+\.)?imdb\.com/.*$/
+// @exclude     /^https?://i\.imdb\.com/.*$/
+// @exclude     /^https?://([^.]+\.)?imdb\.com/images/.*$/
+// @require     http://code.jquery.com/jquery-latest.min.js
+// @grant       none
+// ==/UserScript==
---
+++ dist/imdb-tweaks/imdb-tweaks.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        IMDb Tweaks
+// @description Tweaks to the IMDB website. Remove ref query string and make images linkable.
+// @version     1.0.1
+// @updateUrl   https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/imdb-tweaks/imdb-tweaks.meta.js
+// @downloadUrl https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/imdb-tweaks/imdb-tweaks.user.js
+// @include     /^https?://([^.]+\.)?imdb\.com/.*$/
+// @exclude     /^https?://i\.imdb\.com/.*$/
+// @exclude     /^https?://([^.]+\.)?imdb\.com/images/.*$/
+// @require     http://code.jquery.com/jquery-latest.min.js
+// @grant       none
+// ==/UserScript==
+!function(e,t){"use strict";function a(t){var a=e('<a class="dev-page-button dev-page-button-with-text dev-page-download" href="'+t+'"> <i></i> <span class="label">Download Image</span></a>');e(".dev-meta-actions").append(a)}e("a").removeAttr("onclick").removeAttr("data-ga_click_event");var n=e(".dev-view-meta-content .dev-meta-actions .dev-page-download");if(n.length<=0){var i;if(i=e("#gmi-ResViewSizer_fullimg"),0!==i.length)return void a(i.attr("src"));if(i=e(".dev-page-view .dev-view-deviation .dev-content-full"),0!==i.length)return void a(i.attr("src"))}}(window.jQuery);
