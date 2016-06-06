f4c39304bb53989175531551ee31ac77f63cc94a
Bill Parrott <bill@chimericdream.com>
2016-06-05 23:15:17 -0500

    minor tweaks to build process

---
+++ README.md
@@ -0,0 +1,33 @@
+### Index of scripts
+
+**[Auto-Download Instructables](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/auto-download-instructables/)** [![v1.1.1](https://img.shields.io/badge/v-1.1.1-brightgreen.svg)](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/auto-download-instructables/)
+
+Greasemonkey script to automatically navigate to the download page for an Instructable and trigger the download.
+
+-----
+
+**[Clickable Instagram Pictures](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/clickable-instagram-pictures/)** [![v1.0.1](https://img.shields.io/badge/v-1.0.1-brightgreen.svg)](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/clickable-instagram-pictures/)
+
+Greasemonkey script to make Instagram images clickable to view the original.
+
+-----
+
+**[DeviantArt Download Current Image](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/deviantart-download-image/)** [![v1.0.1](https://img.shields.io/badge/v-1.0.1-brightgreen.svg)](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/deviantart-download-image/)
+
+Adds a download button to all DeviantArt images
+
+-----
+
+**[Comic Rocket Science](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/comic-rocket-science/)** [![v0.3.2a](https://img.shields.io/badge/v-0.3.2a-brightgreen.svg)](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/comic-rocket-science/)
+
+Greasemonkey script to add more comic management features to Comic Rocket.
+
+-----
+
+**[Twitter Skip to Image](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/twitter-skip-to-image/)** [![v1.0.1](https://img.shields.io/badge/v-1.0.1-brightgreen.svg)](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/twitter-skip-to-image/)
+
+-----
+
+**[YouTube Playlist Duration](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/youtube-playlist-duration/)** [![v1.0.1](https://img.shields.io/badge/v-1.0.1-brightgreen.svg)](https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/youtube-playlist-duration/)
+
+Greasemonkey script to add the total duration for a playlist to the details section.
--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,155 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+(function() {
+    'use strict';
 
-gulp.task('default', ['scripts', 'meta']);
+    var gulp = require('gulp');
+    var uglify = require('gulp-uglify');
+    var rename = require('gulp-rename');
+    var replace = require('gulp-replace');
+    var scripts = [];
 
-gulp.task('meta', function() {
-    return gulp.src('src/**/*.js')
-        .pipe(setMetaBlock())
-        .pipe(rename(function(path) {
-            path.basename += '.meta';
-        }))
-        .pipe(gulp.dest('dist'));
-});
+    gulp.task('default', ['scripts', 'meta', 'readme']);
 
-gulp.task('scripts', function() {
-    return gulp.src('src/**/*.js')
-        .pipe(uglify())
-        .pipe(addMetaBlock())
-        .pipe(rename(function(path) {
-            path.basename += '.user';
-        }))
-        .pipe(gulp.dest('dist'));
-});
+    gulp.task('meta', function() {
+        return gulp.src('src/**/*.js')
+            .pipe(setMetaBlock())
+            .pipe(rename(function(path) {
+                path.basename += '.meta';
+            }))
+            .pipe(gulp.dest('dist'));
+    });
 
-function prefixStream(prefixText) {
-    var stream = through();
-    stream.write(prefixText);
-    return stream;
-}
+    gulp.task('scripts', ['meta'], function() {
+        scripts.length = 0;
+        return gulp.src('src/**/*.js')
+            .pipe(uglify())
+            .pipe(addMetaBlock())
+            .pipe(rename(function(path) {
+                path.basename += '.user';
+            }))
+            .pipe(gulp.dest('dist'));
+    });
 
-function addMetaBlock() {
-    function transform(file, cb) {
-        file.contents = Buffer.concat([new Buffer(generateMeta(file)), file.contents]);
+    gulp.task('readme', ['scripts'], function() {
+        return gulp.src(['src/README.md'])
+            .pipe(replace('{{INDEX}}', generateMarkdown()))
+            .pipe(gulp.dest('./'));
+    });
 
-        cb(null, file);
+    function generateMarkdown() {
+        var text = '';
+        scripts.forEach(function(s, idx){
+            text += '**[' + s.name + '](' + s.homeUrl + ')** [![v' + s.version + '](https://img.shields.io/badge/v-' + s.version + '-brightgreen.svg)](' + s.homeUrl + ")\n\n";
+            if (s.description !== '') {
+                text += s.description + "\n\n";
+            }
+            if ((idx + 1) < scripts.length) {
+                text += "-----\n\n";
+            }
+        });
+        return text.trim();
     }
 
-    return require('event-stream').map(transform);
-}
+    function addMetaBlock() {
+        function transform(file, cb) {
+            file.contents = Buffer.concat([new Buffer(generateMeta(file)), file.contents]);
 
-function setMetaBlock() {
-    function transform(file, cb) {
-        file.contents = new Buffer(generateMeta(file));
+            cb(null, file);
+        }
 
-        cb(null, file);
+        return require('event-stream').map(transform);
     }
 
-    return require('event-stream').map(transform);
-}
+    function setMetaBlock() {
+        function transform(file, cb) {
+            file.contents = new Buffer(generateMeta(file));
 
-function generateMeta(file) {
-    var json = getPackageJson(file);
-    var re = /^(?:(?:.+[\/\\])[0-9a-zA-Z_-]+[\/\\])*([0-9a-zA-Z_-]+[\/\\][0-9a-zA-Z_-]+)\.js$/;
-    var safePath = file.path.replace(/\\/g, '/');
-    var ghBaseUrl = 'https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/';
+            cb(null, file);
+        }
 
-    var updateUrl = ghBaseUrl + safePath.replace(re, '$1.meta.js');
-    var downloadUrl = ghBaseUrl + safePath.replace(re, '$1.user.js');
+        return require('event-stream').map(transform);
+    }
 
-    var meta = '// ==UserScript==' + "\n"
-             + '// @name        ' + json['nice-name'] + "\n"
-             + '// @description ' + json.description + "\n"
-             + '// @version     ' + json.version + "\n"
-             + '// @updateUrl   ' + updateUrl + "\n"
-             + '// @downloadUrl ' + downloadUrl + "\n";
+    function generateMeta(file) {
+        var json = getPackageJson(file);
+        var buildInfo = getBuildInfo(json['gm-build-info']);
 
-    var buildInfo = json['gm-build-info'];
+        var re = /^(?:(?:.+[\/\\])[0-9a-zA-Z_-]+[\/\\])*(([0-9a-zA-Z_-]+)[\/\\][0-9a-zA-Z_-]+)\.js$/;
+        var safePath = file.path.replace(/\\/g, '/');
+        var ghBaseUrl = 'https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/';
 
-    if (typeof buildInfo['urls'] !== 'undefined') {
-        buildInfo.urls.forEach(function(url) {
+        var updateUrl = ghBaseUrl + safePath.replace(re, '$1.meta.js');
+        var downloadUrl = ghBaseUrl + safePath.replace(re, '$1.user.js');
+        var homeUrl = ghBaseUrl + safePath.replace(re, '$2/');
+
+        var script = {
+            'name':        json['nice-name'],
+            'description': json['description'],
+            'version':     json['version'],
+            'updateUrl':   updateUrl,
+            'downloadUrl': downloadUrl,
+            'homeUrl':     homeUrl,
+            'urls':        buildInfo['urls'],
+            'excludes':    buildInfo['excludes'],
+            'require':     buildInfo['require'],
+            'grant':       buildInfo['grant']
+        };
+
+        scripts.push(script);
+
+        var meta = '// ==UserScript==' + "\n"
+                 + '// @name        ' + script.name + "\n"
+                 + '// @description ' + script.description + "\n"
+                 + '// @version     ' + script.version + "\n"
+                 + '// @updateUrl   ' + script.updateUrl + "\n"
+                 + '// @downloadUrl ' + script.downloadUrl + "\n";
+
+        script.urls.forEach(function(url) {
             meta += '// @include     ' + url + "\n";
         });
-    }
 
-    if (typeof buildInfo['excludes'] !== 'undefined') {
-        buildInfo.excludes.forEach(function(url) {
+        script.excludes.forEach(function(url) {
             meta += '// @exclude     ' + url + "\n";
         });
-    }
-
-    if (typeof buildInfo['require'] !== 'undefined') {
-        buildInfo.require.forEach(function(url) {
+        script.require.forEach(function(url) {
             meta += '// @require     ' + url + "\n";
         });
-    }
-
-    if (typeof buildInfo['require'] !== 'undefined' && buildInfo.grant.length > 0) {
-        buildInfo.grant.forEach(function(url) {
+        script.grant.forEach(function(url) {
             meta += '// @grant       ' + url + "\n";
         });
-    } else {
-        meta += '// @grant       none' + "\n";
+
+        meta += '// ==/UserScript==' + "\n";
+
+        return meta;
     }
-    meta += '// ==/UserScript==' + "\n";
 
-    return meta;
-}
+    function getPackageJson(file) {
+        var path = file.path.replace(/^(.+[\/\\])[0-9a-zA-Z_-]+\.js$/, '$1package.json');
+        var json = require(path);
 
-function getPackageJson(file) {
-    var path = file.path.replace(/^(.+[\/\\])[0-9a-zA-Z_-]+\.js$/, '$1package.json');
-    var json = require(path);
+        return json;
+    }
 
-    return json;
-}
+    function getBuildInfo(json) {
+        var info = {
+            'urls':     [],
+            'excludes': [],
+            'require':  [],
+            'grant':    ['none']
+        };
+
+        if (typeof json['urls'] !== 'undefined') {
+            info['urls'] = json['urls'];
+        }
+        if (typeof json['excludes'] !== 'undefined') {
+            info['excludes'] = json['excludes'];
+        }
+        if (typeof json['require'] !== 'undefined') {
+            info['require'] = json['require'];
+        }
+        if (typeof json['grant'] !== 'undefined') {
+            info['grant'] = json['grant'];
+        }
+
+        return info;
+    }
+}());
