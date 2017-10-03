6e5e01a6be772c4356d100d8022aaf941c9cea7a
Bill Parrott <william.parrott@springventuregroup.com>
2017-10-03 16:45:27 -0500

    [feature/plex-md] alpha version of plex-md

---
+++ dist/plex-md/plex-md.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        PlexMD
+// @version     1.0.0a
+// @description Greasemonkey script to parse item descriptions via Markdown in Plex Media Server.
+// @updateUrl   https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/plex-md/plex-md.meta.js
+// @downloadUrl https://github.com/chimericdream/GreasemonkeyScripts/raw/master/dist/plex-md/plex-md.user.js
+// @include     https://app.plex.tv/*
+// @require     https://cdn.rawgit.com/showdownjs/showdown/1.7.4/dist/showdown.min.js
+// @require     http://code.jquery.com/jquery-3.2.1.min.js
+// @grant       none
+// ==/UserScript==
+!function(e,a,n){"use strict";var r=debounce(function(){var a=e('[class*="PrePlaySummary-summary-"] > [class*="CollapsibleText-contentTransition-"] > [class*="Measure-container-"]');if(0===a.length&&(a=e('[class*="PrePlaySummary-summary-"] > .false > [class*="Measure-container-"]')),0!==a.length){var r=a.html();if(r=r.replace(/<\/p><p>/gi,"\n").replace(/<\/?p>/gi,"").replace(/<div.+><\/div>/gi,""),!a.hasClass("pmd-markdownified")){var s=new n.Converter,i=s.makeHtml(r);e("p",a).remove(),a.contents().filter(function(){return 3==this.nodeType}).remove(),a.prepend(i),a.addClass("pmd-markdownified")}}},2e3);a.addEventListener("DOMNodeInserted",r,!1)}(jQuery,window,window.showdown);
---
+++ src/plex-md/plex-md.js
@@ -0,0 +1,38 @@
+(function($, win, showdown) {
+    'use strict';
+
+    var parseMd = debounce(function() {
+        var $parent = $('[class*="PrePlaySummary-summary-"] > [class*="CollapsibleText-contentTransition-"] > [class*="Measure-container-"]');
+
+        if ($parent.length === 0) {
+            $parent = $('[class*="PrePlaySummary-summary-"] > .false > [class*="Measure-container-"]');
+        }
+
+        if ($parent.length === 0) {
+            return;
+        }
+
+        var text = $parent.html();
+
+        text = text.replace(/<\/p><p>/ig, "\n")
+                   .replace(/<\/?p>/ig, '')
+                   .replace(/<div.+><\/div>/ig, '');
+
+        if ($parent.hasClass('pmd-markdownified')) {
+            return;
+        }
+
+        var converter = new showdown.Converter();
+        var html = converter.makeHtml(text);
+
+        $('p', $parent).remove();
+        $parent.contents().filter(function() {
+            return (this.nodeType == 3);
+        }).remove();
+
+        $parent.prepend(html);
+        $parent.addClass('pmd-markdownified');
+    }, 2000);
+
+    win.addEventListener('DOMNodeInserted', parseMd, false);
+}(jQuery, window, window.showdown));
